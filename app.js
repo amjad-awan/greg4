9a9831d2213b96fa4ff876123a550a24e7fe34d3
amjad-awan <73718759+amjad-awan@users.noreply.github.com>
2021-12-09 01:57:21 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+
+input = document.querySelector("#phone");
+            window.intlTelInput(input, {
+                separateDialCode: true,
+                excludeCountries: ["in", "il"],
+                preferredCountries: ["ru", "jp", "pk", "no"]
+            });
